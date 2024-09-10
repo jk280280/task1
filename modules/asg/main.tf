c248abee4b6754eaa3b606873a3b10229bd974a9
jk280280 <38111277+jk280280@users.noreply.github.com>
2024-09-10 15:37:07 +0530

    Update main.tf

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -2,8 +2,8 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_security_group" "this" {
-  name        = "this-sg"
+resource "aws_security_group" "app" {
+  name        = "app-sg"
   description = "Allow inbound SSH and other traffic"
   vpc_id      = data.aws_vpc.default.id
 
@@ -23,17 +23,17 @@ egress {
   # Add other rules as necessary
 
   tags = {
-    Name = "asg-sg"
+    Name = "app-asg-sg"
   }
 }
 
 
-resource "aws_launch_configuration" "this" {
+resource "aws_launch_configuration" "app" {
   name          = "ec2-launch-configuration"
   image_id      = "ami-02c21308fed24a8ab"
   instance_type = "t3.micro"
   key_name       = "tasks"
-  security_groups = [aws_security_group.this.id]
+  security_groups = [aws_security_group.app.id]
   associate_public_ip_address = true
 
   lifecycle {
@@ -42,14 +42,14 @@ resource "aws_launch_configuration" "this" {
 }
 
 
-resource "aws_autoscaling_group" "this" {
-  launch_configuration = aws_launch_configuration.this.id
+resource "aws_autoscaling_group" "app" {
+  launch_configuration = aws_launch_configuration.app.id
   min_size             = 2
   max_size             = 2
   vpc_zone_identifier  = var.subnet_ids
   tag {
     key                 = "Name"
-    value               = "auto-scaling-instance"
+    value               = "app-auto-scaling-instance"
     propagate_at_launch = true
   }
   health_check_type             = "EC2"
@@ -65,12 +65,12 @@ resource "aws_autoscaling_policy" "scale_out" {
   name                   = "scale-out"
   scaling_adjustment     = 1
   adjustment_type        = "ChangeInCapacity"
-  autoscaling_group_name = aws_autoscaling_group.this.name
+  autoscaling_group_name = aws_autoscaling_group.app.name
 }
 
 resource "aws_autoscaling_policy" "scale_in" {
   name                   = "scale-in"
   scaling_adjustment     = -1
   adjustment_type        = "ChangeInCapacity"
-  autoscaling_group_name = aws_autoscaling_group.this.name
+  autoscaling_group_name = aws_autoscaling_group.app.name
 }
