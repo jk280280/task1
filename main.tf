bcd33d4cd1315ff271f425cf7c11cada2a8b8b80
jk280280 <38111277+jk280280@users.noreply.github.com>
2024-09-11 19:14:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,10 +24,4 @@ module "asg" {
   lb_arn         = module.load_balancer.lb_arn
 }
 
-output "lb_dns_name" {
-  value = module.load_balancer.lb_dns_name
-}
-output "asg_name" {
-  value = module.asg.asg_name
-}
 
